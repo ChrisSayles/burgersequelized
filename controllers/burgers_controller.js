3d0d8c1f9eef3ede385bbc5bfc7f92a90b951370
Chris Sayles <chrissayles@Chriss-MacBook-Pro-2.local>
2017-02-22 15:09:28 -0600

    finished app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,47 @@
 var express = require("express");
 var router = express.Router();
-var models = require("../models");
 
-//route will be set to /burgers by default
-router.get('/', function(req, res) {
-    res.redirect('/burgers');
+//data model
+var db = require("../models");
+
+//get using seq findAll method
+router.get("/", function(req, res){
+  db.burgers.findAll({
+    order: "burger_name DESC",
+  }).then(function (data){
+    var burgerObj = {
+      burgers: data
+    };
+    res.render("index", burgerObj);
+  });
 });
 
-router.get('/burgers', function(req, res) {
-    models.burgers.findAll()
-        .then(function(data) {
-            var hbsObject = { burgers: data };
-            res.render("index", hbsObject);
-        });
+//Post seq create with name
+router.post("/burgers/create", function(req, res){
+  console.log(req.body);
+  var burgerName = req.body.burger_name;
+  db.burgers.create({
+    burger_name: burgerName
+  }).then(function (){
+    res.redirect("/");
+  });
 });
 
-router.post('/burgers/create', function(req, res) {
-    models.burgers.create({
-        burger_name: req.body.name,
-    }).then(function() {
-        res.redirect('/');
-    });
-});
-
-
-
-router.put("/burgers/update/:id", function(req,res){
-  models.burgers.update({
-    devoured: true
+//put route -->back to index
+router.put("/burgers/update", function(req, res){
+  console.log("Request Body Below");
+  console.log(req.body);
+  db.burgers.update({
+    devoured: true,
   },{
     where: {
-      id: req.params.id
+      id: req.body.burger_id
     }
-  }).then(function(data){
-    res.redirect("/burgers");
+  }).then(function(){
+    res.redirect("/");
   });
-  });
+});
 
 
-
-// Export routes for server.js to use.
+//export routes for server.js
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
-var PORT = 3000;
-
+var db = require("./models");
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -25,12 +23,15 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
-app.use("/burgers/update/:id", routes);
-app.use('/burgers/create', routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
+//listen on port 8080
+var PORT = process.env.PORT || 8080;
 
-// db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-// });
+//Syn our sequelize models and then starting our express.app
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function(){
+		console.log("App is listening on PORT " + PORT);
+	});
+});
